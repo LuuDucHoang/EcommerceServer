1155886dae510053ffe9f1d3795b909d51e7f04e
LuuDucHoang <livetolove2736@gmail.com>
2023-06-16 22:04:33 +0700

    Chuyển firebase config sang env

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 module.exports = {
     firebaseConfig: {
-        apiKey: 'AIzaSyARCRnJ2ZDSlY08RunWSzRd1sDClVS4WoA',
-        authDomain: 'imgbase-431f8.firebaseapp.com',
-        projectId: 'imgbase-431f8',
-        storageBucket: 'imgbase-431f8.appspot.com',
-        messagingSenderId: '704210187187',
-        appId: '1:704210187187:web:299640932968f8088a11c6',
-        measurementId: 'G-8F32EEBM7L',
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
     },
 };
